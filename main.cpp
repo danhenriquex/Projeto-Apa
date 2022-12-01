e3b6a244d81332c09c98f6ece97842fc21afcdf6
Ana Beatriz <danilohssantana@gmail.com>
2022-11-30 21:39:09 -0300

    adding instances

--- main.cpp
+++ main.cpp
@@ -9,31 +9,61 @@ using namespace std;
 
 class Gift {
   public:
-    int id;
-    int weight;
+    Gift( void ) {};
+    Gift( const int& id, const int& weight ) {
+      this->id_ = id;
+      this->weight_ = weight;
+    }
+
+    int id_;
+    int weight_;
 };
 
 class Treno {
   public:
-    int capacity;
-    std::vector<Gift> gifts;
+    int capacity_;
+    std::vector<Gift> gifts_;
 };
 
 class Optimization {
   public:
-    int oi;
+    int totalGifts_;
+    int totalTrenos_;
+    int trenoCapacity_;
+    int elementsL_;
+    std::vector<Gift> allGifts_;
 };
 
 int main( void ) {
   auto readFile = []( const std::string& file ) {
     std::ifstream text(file);
 
+    Optimization opt = Optimization();
+
+    bool isGift = true;
+    bool isTreno = false;
+    bool isCapacity = false;
+
     if ( text.is_open() ) {
       std::string value;
 
       while( !text.eof() ) {
         std::getline(text, value);
-        std::cout << "value: " << value << std::endl;
+
+        if ( isGift ) {
+          for ( int i = 1; i <= stoi(value); ++i ) {
+            Gift gift = Gift( i, -1 );
+            opt.allGifts_.push_back( gift );
+          }
+
+          isGift = false;
+          isTreno = true;
+        }
+
+        if ( isTreno ) {
+
+        }
+
       }
     }
   };
